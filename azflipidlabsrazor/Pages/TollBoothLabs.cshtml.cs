a7940329462cc6510743ddf2d6e90b28c8566b11
Johannes Åkerman <johannes.akerman@gmail.com>
2018-04-24 16:52:58 +0200

    First commit

---
+++ azflipidlabsrazor/Pages/TollBoothLabs.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AZFLIP_IDLABS.Pages
+{
+    public class TollBoothLabsModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Front-End environment for AzFlip Functions";
+        }
+    }
+}
