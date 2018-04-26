05ddaebd2d349e78468bc7496cce40d23b426096
Johannes Åkerman <johannes.akerman@gmail.com>
2018-04-27 01:43:34 +0200

    com

---
+++ azflipidlabsrazor/Controllers/FileUploadController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace azflipidlabsrazor.Controllers
+{
+    [Route("api/[controller]")]
+
+    public class FileUploadController : Controller
+    {
+        [HttpPost("[Action]")]
+        async public Task<IActionResult> SaveFile(IFormFile files)
+        {
+            // Connect to Azure
+            // Set the connection string
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=azfltollboothblob001;AccountKey=zFEwiuow6NW/gINn+xsxzBDdvRso2Q7+JBSAp55MEafnV1p04oR/fxalt7sg0c5AtEyW5LN5qNVeLpg+obxviw==;EndpointSuffix=core.windows.net");
+
+            // Create a blob client. 
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Get a reference to a container  
+            CloudBlobContainer container = blobClient.GetContainerReference("images");
+
+            // Save file to blob
+            // Get a reference to a blob  
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(files.FileName);
+
+            // Create or overwrite the blob with the contents of a local file 
+            using (var fileStream = files.OpenReadStream())
+            {
+                await blockBlob.UploadFromStreamAsync(fileStream);
+            }
+
+            //return View();
+            // Respond with success
+            return Json(new
+            {
+                name = blockBlob.Name,
+                uri = blockBlob.Uri,
+                size = blockBlob.Properties.Length
+            });
+        }
+    }
+}
--- azflipidlabsrazor/Pages/TollBoothLabs.cshtml.cs
+++ azflipidlabsrazor/Pages/TollBoothLabs.cshtml.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using System.IO;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
-namespace AZFLIP_IDLABS.Pages
+namespace azflipidlabsrazor.Pages
 {
     public class TollBoothLabsModel : PageModel
     {
+        public string FileName { get; set; }
+        public string AzureUrl { get; set; }
         public string Message { get; set; }
+        public List<TollBoothLabsModel> FileList { get; set; }
 
         public void OnGet()
         {
             Message = "Front-End environment for AzFlip Functions";
+
+        }
+
+        public async Task<List<Uri>> GetFullBlobsAsync()
+        {
+            // Connect to Azure
+            // Set the connection string
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=azfltollboothblob001;AccountKey=zFEwiuow6NW/gINn+xsxzBDdvRso2Q7+JBSAp55MEafnV1p04oR/fxalt7sg0c5AtEyW5LN5qNVeLpg+obxviw==;EndpointSuffix=core.windows.net");
+
+            // Create a blob client. 
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Get a reference to a container  
+            CloudBlobContainer container = blobClient.GetContainerReference("images");
+
+            var blobList = await container.ListBlobsSegmentedAsync(string.Empty, true, BlobListingDetails.None, int.MaxValue, null, null, null);
+
+            var FileList = blobList;
+
+            return (from blob in blobList.Results where !blob.Uri.Segments.LastOrDefault().EndsWith("-thumb") select blob.Uri).ToList();
         }
     }
 }
